82827e784ba79331105e2fc4899ea33fc8e7ed2c
carlnikitin <carl.nikitin@khk.ee>
2020-01-07 13:18:10 +0200

    Background fixed

--- nordic-gifts-project/src/App.vue
+++ nordic-gifts-project/src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
     <link href="https://fonts.googleapis.com/css?family=Charm&display=swap" rel="stylesheet">
-<<<<<<< HEAD
-    <img style="margin-top: 25px; width: 150px" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vali sobivad elemendid, et luua meelepärane suveniir" />
-     <img style="margin-top: 45px; width: 320px" alt="Nordic gifts logo" src="./assets/elf.png" />
-    <section id="section05" class="demo">
-      <a href="#"><span></span>.......</a>
-    </section>
-    <textarea rows="7" v-model="message" placeholder="add multiple lines"></textarea>
-=======
     <img style="margin-top: 25px !important; width: 250px" alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld style="margin-top:20px !important" msg="Vali sobivad elemendid, et luua meelepärane suveniir" />
     <img style="margin-top: 45px !important; width: 320px" alt="Nordic gifts logo" src="./assets/elf.png" />
@@ -25,7 +16,6 @@
       </a>
     </div>
     <textarea rows="7" v-model="message" placeholder="Add multiple lines"></textarea>
->>>>>>> 069beba13055afa6582a8892d3ae10585ac52f8d
   </div>
 </template>
 
@@ -65,63 +55,10 @@ html, body {
   display: block;
 }
 /*Here starts the arrow pointing down animation style*/
-<<<<<<< HEAD
-section {
-  position: relative;
-  width: 100%;
-}
-
-.demo a {
-  position: absolute;
-  left: 50%;
-  top: 150px;
-  z-index: 2;
-  display: inline-block;
-  -webkit-transform: translate(0, -50%);
-  transform: translate(0, -50%);
-  color: transparent;
-  font : normal 400 20px/1 'Josefin Sans', sans-serif;
-  letter-spacing: .1em;
-  text-decoration: none;
-  transition: opacity .3s;
-}
-
-#section05 a {
-  padding-top: 10px;
-}
-#section05 a span {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  width: 24px;
-  height: 24px;
-  margin-left: -12px;
-  border-left: 1.5px solid #333;
-  border-bottom: 1.5px solid #333;
-  -webkit-transform: rotate(-45deg);
-  transform: rotate(-45deg);
-  -webkit-animation: sdb05 1.5s infinite;
-  animation: sdb05 1.5s infinite;
-  box-sizing: border-box;
-}
-@-webkit-keyframes sdb05 {
-  0% {
-    -webkit-transform: rotate(-45deg) translate(0, 0);
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: rotate(-45deg) translate(-20px, 20px);
-    opacity: 0;
-  }
-=======
 
 .wrapper {
 	width: 0%;
 	height: 20%;
->>>>>>> 069beba13055afa6582a8892d3ae10585ac52f8d
 }
 
 .arrow {
@@ -191,12 +128,8 @@ border: solid 1.5px #707070;
 font-family: 'Charm', cursive;
 font-size: 20px;
 width: 35%;
-<<<<<<< HEAD
-margin-top: 150px;
-=======
 display: inline-block;
 margin-top: 300px !important;
->>>>>>> 069beba13055afa6582a8892d3ae10585ac52f8d
 }
 textarea::-webkit-input-placeholder {
 font-family: 'Charm', cursive;
