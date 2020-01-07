33dd85393b8be6e4b3cbfb281f06c64005fd212a
carlnikitin <carl.nikitin@khk.ee>
2020-01-07 09:40:48 +0200

    Added right font

--- nordic-gifts-project/src/App.vue
+++ nordic-gifts-project/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <link href="https://fonts.googleapis.com/css?family=Charm&display=swap" rel="stylesheet">
     <img style="width: 250px" alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Vali sobivad elemendid, et luua meelepärane suveniir" />
   </div>
@@ -18,13 +19,12 @@ export default {
 
 <style>
 #app {
-  font-family: "Apple Chancery", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  font-family: 'Charm', cursive;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #707070;
   margin-top: 60px;
-  font-weight: 300;
-  font-size: 14px;
+  font-weight: 100;
+  font-size: 13px;
 }
 </style>
