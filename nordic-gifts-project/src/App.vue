6e2b8010cf70aff5eedbc04a2248dd6003bd75ce
rkivi <raul-markus.kivisalu@khk.ee>
2020-01-08 10:02:09 +0200

    contact form

--- nordic-gifts-project/src/App.vue
+++ nordic-gifts-project/src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <link href="https://fonts.googleapis.com/css?family=Charm&display=swap" rel="stylesheet">
-    <img style="margin-top: 25px !important; width: 250px" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld style="margin-top:20px !important" msg="Vali sobivad elemendid, et luua meelepärane suveniir" />
-    <img style="margin-top: 45px !important; width: 320px" alt="Nordic gifts logo" src="./assets/elf.png" />
-    <br>
+    <link
+      href="https://fonts.googleapis.com/css?family=Charm&display=swap"
+      rel="stylesheet"
+    />
+    <img
+      style="margin-top: 25px !important; width: 250px"
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
+    <HelloWorld
+      style="margin-top:20px !important"
+      msg="Vali sobivad elemendid, et luua meelepärane suveniir"
+    />
+    <img
+      style="margin-top: 45px !important; width: 320px"
+      alt="Nordic gifts logo"
+      src="./assets/elf.png"
+    />
+    <br />
     <VueNextLevelScroll target="#text">
-    <div class="wrapper">
-      <button type=button>
-      <div class="arrow">
-      <ul>
-        <li></li>
-        <li></li>
-      </ul>
+      <div class="wrapper">
+        <button type="button">
+          <div class="arrow">
+            <ul>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </button>
       </div>
-      </button>
-    </div>
     </VueNextLevelScroll>
-    <textarea id="text" rows="7" v-model="message" placeholder="Add multiple lines"></textarea>
+    <textarea
+      id="text"
+      rows="7"
+      v-model="message"
+      placeholder="Add multiple lines"
+    ></textarea>
+    <input type="text" placeholder="Ees -ja Perekonnanimi" id="name" />
+    <input type="text" placeholder="Telefoni nr." id="tel" />
+    <input type="text" placeholder="E-Mail" id="email" />
+    <input type="submit" value="Saada">
   </div>
 </template>
 
 <script>
-import VueNextLevelScroll from "vue-next-level-scroll/"
+import VueNextLevelScroll from "vue-next-level-scroll/";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
@@ -32,7 +55,6 @@ export default {
     VueNextLevelScroll
   }
 };
-
 </script>
 
 <style>
@@ -42,13 +64,14 @@ export default {
   margin: 0px !important;
 }
 
-html, body {
+html,
+body {
   height: 100%;
-  position:relative;
+  position: relative;
 }
 /*General style*/
 #app {
-  font-family: 'Charm', cursive;
+  font-family: "Charm", cursive;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #707070;
@@ -63,98 +86,99 @@ html, body {
 /*Here starts the arrow pointing down animation style*/
 
 .wrapper {
-	width: 0%;
-	height: 20%;
+  width: 0%;
+  height: 20%;
 }
 
 .arrow {
-	width: 38px;
-	height: 14px;
-	margin: auto !important;
-	text-transform: uppercase;
-	letter-spacing: 3px;
-	text-align: center;
-	position: absolute;
-	left: 0;
-	right: 0;
-	bottom: 20px;
-	opacity: 0.5;
-	transition: ease 1s;
-	animation: bouncey 1.6s linear infinite;
+  width: 38px;
+  height: 14px;
+  margin: auto !important;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 20px;
+  opacity: 0.5;
+  transition: ease 1s;
+  animation: bouncey 1.6s linear infinite;
 }
 
 .arrow ul {
-	list-style: none;
+  list-style: none;
 }
 
 .arrow ul li {
-	width: 24px;
-	height: 4px;
-	background: #333;
+  width: 24px;
+  height: 4px;
+  background: #333;
 }
 
 .arrow ul li:nth-child(1) {
-	left: 0;
-	top: 0;
-	position: absolute;
-	transform: rotate(45deg);
-	border-radius: 5px 0 0 5px;
+  left: 0;
+  top: 0;
+  position: absolute;
+  transform: rotate(45deg);
+  border-radius: 5px 0 0 5px;
 }
 
 .arrow ul li:nth-child(2) {
-	left: 14px;
-	position: absolute;
-	border-radius: 0 5px 5px 0;
-	top: 0;
-	transform: rotate(-45deg);
+  left: 14px;
+  position: absolute;
+  border-radius: 0 5px 5px 0;
+  top: 0;
+  transform: rotate(-45deg);
 }
 
 .arrow:hover {
-	opacity: 1;
+  opacity: 1;
 }
 
 @keyframes bouncey {
-	0% {
-		transform: translateY(0);
-	}
+  0% {
+    transform: translateY(0);
+  }
 
-	50% {
-		transform: translateY(4px);
-	}
+  50% {
+    transform: translateY(4px);
+  }
 
-	100% {
-		transform: translateY(0);
-	}
+  100% {
+    transform: translateY(0);
+  }
 }
-
 
 /*Textarea style*/
 textarea {
-border: solid 1.5px #707070;
-font-family: 'Charm', cursive;
-font-size: 20px;
-width: 35%;
-display: inline-block;
-margin-top: calc(100vh - 450px) !important;
+  border: solid 1.5px #707070;
+  font-family: "Charm", cursive;
+  font-size: 20px;
+  width: 35%;
+  display: inline-block;
+  margin-top: calc(100vh - 450px) !important;
 }
 textarea::-webkit-input-placeholder {
-font-family: 'Charm', cursive;
-font-size: 20px;
+  font-family: "Charm", cursive;
+  font-size: 20px;
 }
 
-textarea:-moz-placeholder { /* Firefox 18- */
-font-family: 'Charm', cursive; 
-font-size: 20px;
+textarea:-moz-placeholder {
+  /* Firefox 18- */
+  font-family: "Charm", cursive;
+  font-size: 20px;
 }
 
-textarea::-moz-placeholder {  /* Firefox 19+ */
-font-family: 'Charm', cursive;  
-font-size: 20px;
+textarea::-moz-placeholder {
+  /* Firefox 19+ */
+  font-family: "Charm", cursive;
+  font-size: 20px;
 }
 
 textarea:-ms-input-placeholder {
-font-family: 'Charm', cursive;  
-font-size: 20px;
+  font-family: "Charm", cursive;
+  font-size: 20px;
 }
 
 /*Mobile view*/
@@ -167,5 +191,4 @@ font-size: 20px;
     bottom: 20px;
   }
 }
-
 </style>
