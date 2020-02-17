a1da8f81871512ee306e6b4e1f54e0e3ade178b4
carlnikitin <carl.nikitin@khk.ee>
2020-02-17 14:38:37 +0200

    Arrow removed

--- Nordic_gifts/src/components/HomeComponent.vue
+++ Nordic_gifts/src/components/HomeComponent.vue
@@ -22,11 +22,11 @@
 
 
     <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <img border="0" alt="W3Schools" src="../assets/Estonia.png" width="30" height="20">
+      <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <img border="0" src="../assets/Estonia.png" width="30" height="20">
       </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="http://localhost:8081/eng"><img border="0" alt="W3Schools" src="../assets/Great_Britain.png" width="30" height="20"></a>
+          <a class="dropdown-item" href="http://localhost:8081/eng"><img border="0" src="../assets/Great_Britain.png" width="30" height="20"></a>
         </div>
     </div>
 
